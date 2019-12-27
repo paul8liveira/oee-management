1c3bfd6490cd1b65d96b98f693fbe7d237b10573
Paulo Oliveira <paulooliveira@pecege.com>
2019-12-27 14:55:34 -0300

    alinhamento das pausas na aba de produção

--- src/app/components/machine.production/machine.production.component.ts
+++ src/app/components/machine.production/machine.production.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewContainerRef, OnDestroy } from '@angular/core';
 import { BaseComponent } from '../base.component';
 import { ToastsManager } from 'ng2-toastr/src/toast-manager';
 import { FilterService } from '../../services/dashboard/filter.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-machine-production',
@@ -11,12 +12,14 @@ import { FilterService } from '../../services/dashboard/filter.service';
 export class MachineProductionComponent extends BaseComponent implements OnInit, OnDestroy {
   private intervalTimer: any;
   public timerStr: string = "00:00:00";
+  private unsubscribe: Subscription[] = [];
   
   constructor(
     public toastr: ToastsManager, 
     private filterService: FilterService) 
   {
-    super();              
+    super();
+    this.listenFilters();              
   }
 
   ngOnInit() {
@@ -25,7 +28,8 @@ export class MachineProductionComponent extends BaseComponent implements OnInit,
 
   ngOnDestroy() {
     clearInterval(this.intervalTimer);
-  }
+    this.unsubscribe.forEach(f => f.unsubscribe());
+  } 
 
   private startIntervalTimer() {
     let sec = 60;
@@ -43,4 +47,20 @@ export class MachineProductionComponent extends BaseComponent implements OnInit,
         this.timerStr = this.secToTime(sec);
       }, 1000);
   }  
+
+  private stopIntervalTimer() {
+    this.timerStr = 'pausado';
+    clearInterval(this.intervalTimer);
+  }
+
+  private listenFilters() {
+		const subsDWMY = this.filterService.onDWMYUpdate$.subscribe(dwmy => {      
+      //diferente de dia atual, trava o countdown
+      if(dwmy != 3) 
+        this.stopIntervalTimer();
+      else 
+        this.startIntervalTimer();
+    });
+		this.unsubscribe.push(subsDWMY);    
+  }   
 }
